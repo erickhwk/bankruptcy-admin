ba37abbabaf224884d4e482e2e30951c4c8a8dca
Erick Valente <erickhwk@gmail.com>
2024-07-06 16:35:30 -0300

    add team page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def team
+    @team = User.where(tenancy_id: current_user.tenancy_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :reports
   resources :lawsuits
   root 'pages#index'
-  
+
+  get 'team', to: 'pages#team'
+
   resources :tenancies
   devise_for :users
 
