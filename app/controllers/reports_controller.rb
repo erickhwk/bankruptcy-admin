387fad22d433366fe3c0539428d164668e497388
Erick Valente <erickhwk@gmail.com>
2024-07-06 17:23:50 -0300

    change reports layout to table

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,7 +3,7 @@ class ReportsController < ApplicationController
 
   # GET /reports or /reports.json
   def index
-    @reports = Report.all
+    @reports = Report.all.order(created_at: :desc)
   end
 
   # GET /reports/1 or /reports/1.json
--- app/models/lawsuit.rb
+++ app/models/lawsuit.rb
@@ -1,3 +1,4 @@
 class Lawsuit < ApplicationRecord
   belongs_to :tenancy
+  has_many :reports, dependent: :destroy
 end
