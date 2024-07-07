8022832e44885d88e5ebe20d10ed4a1a1cf43101
Erick Valente <erickhwk@gmail.com>
2024-07-06 21:19:49 -0300

    add resume tables to index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def index
+    @reports = Report.last(10).reverse
+    @lawsuits = Lawsuit.where(tenancy_id: current_user.tenancy_id).last(10).reverse
+    @companies = Company.last(10).reverse
   end
 
   def team
